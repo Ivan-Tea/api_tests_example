41bcb3691efcfcd5709912023b4e6c8d05222e18
i.mokryakov <i.mokryakov@tinkoff.ru>
2023-11-15 15:57:38 +0400

    kek

--- api_data/data_lists.py
+++ api_data/data_lists.py
@@ -10,23 +10,38 @@ def make_url(id_list, api_key, api_token):
 
 def get_list(id_list, api_key, api_token):
     """Get a list from the API"""
-    response = requests.get(url=make_url(id_list, api_key, api_token), timeout=10)
+    response = requests.get(
+        url=make_url(id_list, api_key, api_token),
+        timeout=10
+    )
     return response
 
 
 def update_list(id_list, data, api_key, api_token):
     """Update a list from the API"""
-    request = requests.put(url=make_url(id_list, api_key, api_token), data=data, timeout=10)
+    request = requests.put(
+        url=make_url(id_list, api_key, api_token),
+        data=data,
+         timeout=10
+    )
     return request
 
 
-def create_list(data, id_list, api_key, api_token):
+def create_list(data, api_key, api_token):
     """Create a list from the API"""
-    request = requests.post(url=make_url(id_list, api_key, api_token), data=data, timeout=10)
+    request = requests.post(
+        url=LISTS_URL+f'?key={api_key}&token={api_token}',
+        data=data,
+        timeout=10
+    )
     return request
 
 
 def archive_a_list(id_list, value, api_key, api_token):
     """Archive a list from the API"""
-    request = requests.put(url=make_url(id_list, api_key, api_token), params=value, timeout=10)
+    request = requests.put(
+        url=make_url(id_list, api_key, api_token),
+        params=value,
+        timeout=10
+    )
     return request
